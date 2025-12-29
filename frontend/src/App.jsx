20d2ab6415045a8ff45f8091d1646fe88379c0d5
Karan gupta <gupta.karan1.gh@gmail.com>
2025-12-29 20:23:56 +0530

    frontend setup done

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import './index.css'
+
+function App() {
+  return (
+    <>
+      <h1 className='font-bold underline text-9xl'>Hello World</h1>
+    </>
+  )
+}
+
+export default App
